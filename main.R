2f493526c5728f410ae5feae077a3e553ec02e10
salamandery <sp.ront@hotmail.com>
2024-05-08 00:40:17 -0300

    first commit

---
+++ main.R
@@ -0,0 +1,27 @@
+# Aprendendo R 
+
+# Equações básicas
+2+2
+3-1
+4/2
+3*3
+
+# Criar objeto
+obj = 2+3
+
+obj2 <- 2+3
+
+# Criar sequencia numérica
+sequencia <- seq(1, 1000, by=10)
+sequencia
+
+# Funções básicas
+
+mean(sequencia)
+median(sequencia)
+min(sequencia)
+max(sequencia)
+
+# sumário
+summary(sequencia)
+
